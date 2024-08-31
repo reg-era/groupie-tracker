43d54176f4476dd9c1302a4a409d40d27157c99f
reg-era <ilyass.med.fkh@gmail.com>
2024-08-31 14:58:53 +0100

    minimalize time

--- tracker/api_process.go
+++ tracker/api_process.go
@@ -11,9 +11,6 @@ type Info struct {
 	Data interface{}
 }
 
-// if all data fetched return true
-var Stat bool = false
-
 // processe the fetching from Api
 func APiProcess(url string) {
 	var wgp sync.WaitGroup
@@ -23,28 +20,23 @@ func APiProcess(url string) {
 	Get_Api_Data(Info{Api.Artists, &Artists})
 
 	wgp.Add(1)
-	go func(status *bool) {
+	go func() {
 		defer wgp.Done()
-		wgp.Add(len(Artists) * 3)
-		// for each artist fetching date location reletion
+		// for each artist fetching date location relation
 		for i := range Artists {
 			artist := &Artists[i]
 			go func(artist *Artist) {
-				defer wgp.Done()
 				Get_Api_Data(Info{Api.Dates + "/" + strconv.Itoa(artist.Id), &artist.DateST})
 			}(artist)
 
 			go func(artist *Artist) {
-				defer wgp.Done()
 				Get_Api_Data(Info{Api.Locations + "/" + strconv.Itoa(artist.Id), &artist.LocationST})
 			}(artist)
 
 			go func(artist *Artist) {
-				defer wgp.Done()
 				Get_Api_Data(Info{Api.Relation + "/" + strconv.Itoa(artist.Id), &artist.RelationST})
 			}(artist)
 		}
-		*status = true
-	}(&Stat)
+	}()
 	wgp.Wait()
 }
