e07f2f8f47fe452a15a03d416e0a06a4a75698de
x3alone <otmanchouarix3@gmail.com>
2024-09-07 18:22:14 +0100

    fluky

--- webserver/Feature_filter.go
+++ webserver/Feature_filter.go
@@ -13,23 +13,12 @@ func FilterProcess(key string) []int {
 	type (
 		newArtist struct {
 			Id           int      `json:"id"`
-			Image        string   `json:"image"`
-			Name         string   `json:"name"`
 			Members      []string `json:"members"`
 			CreationDate int      `json:"creationDate"`
 			FirstAlbum   string   `json:"firstAlbum"`
 			// fetching location
 			LocationST struct {
 				Locations []string `json:"locations"`
-				Dates     string   `json:"dates"`
-			}
-			// fetching date
-			DateST struct {
-				Dates []string `json:"dates"`
-			}
-			// fetching the relation
-			RelationST struct {
-				DatesLocation map[string][]string `json:"datesLocations"`
 			}
 	}
 	)
